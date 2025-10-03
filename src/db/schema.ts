5511daa13fb84b3ab5cf12da4234016a485079a0
wmarchment <wmarchment@mindgruve.com>
2025-10-03 15:31:24 -0700

    Create index of advocate fields and make it searchable via /api/search/[term] route

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 import { sql } from "drizzle-orm";
 import {
+  index,
   pgTable,
   integer,
   text,
@@ -10,15 +11,27 @@ import {
 } from "drizzle-orm/pg-core";
 
 const advocates = pgTable("advocates", {
-  id: serial("id").primaryKey(),
-  firstName: text("first_name").notNull(),
-  lastName: text("last_name").notNull(),
-  city: text("city").notNull(),
-  degree: text("degree").notNull(),
-  specialties: jsonb("payload").default([]).notNull(),
-  yearsOfExperience: integer("years_of_experience").notNull(),
-  phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
-  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
-});
+    id: serial("id").primaryKey(),
+    firstName: text("first_name").notNull(),
+    lastName: text("last_name").notNull(),
+    city: text("city").notNull(),
+    degree: text("degree").notNull(),
+    specialties: jsonb("payload").default([]).notNull(),
+    yearsOfExperience: integer("years_of_experience").notNull(),
+    phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
+    createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
+  },
+  (table) => [
+    index('search_index').using(
+      'gin',
+      sql`( 
+        setweight(to_tsvector('english', ${table.firstName}), 'A') ||
+        setweight(to_tsvector('english', ${table.lastName}), 'A') ||
+        setweight(to_tsvector('english', ${table.city}), 'B') ||
+        setweight(to_tsvector('english', ${table.degree}), 'C') ||
+        setweight(to_tsvector('english', ${table.specialties}), 'B')
+      )`,
+    ),
+  ]);
 
 export { advocates };
